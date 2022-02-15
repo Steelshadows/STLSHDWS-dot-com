086e19ff3b2f4e2d0f5e8de7d8c0c9862067ea87
Steelshadows <its.a.me.brand@gmail.com>
2022-02-15 14:31:41 +0100

    2/15 2:30PM

--- manager/js/managerRolesFunctions.js
+++ manager/js/managerRolesFunctions.js
@@ -4,6 +4,63 @@ function submitUserRoles(){
 function searchUsers(){
     console.log('search');
 }
+function addRole(args){
+    args = JSON.parse(args);
+    console.log('addRole',args);
+    console.log($('*[data-roleid="'+args["role_id"]+'"]').prop('checked'));
+    
+    $checked = $('*[data-roleid="'+args["role_id"]+'"]').prop('checked');
+    $role_ids = $("#edit_role_ids").html().split(",");
+    $index = $role_ids.indexOf(args["role_id"]);
+    if($index != -1){
+        console.log('found role',$index);
+        if($checked == false){
+            $("#edit_role_ids").html($("#edit_role_ids").html() + ","+args["role_id"]);
+        } 
+    }else{
+        console.log('no role');
+        if($checked){
+            $("#edit_role_ids").html($("#edit_role_ids").html() + ","+args["role_id"]);
+        }        
+    }
+
+    //select checked and create string
+    /*
+    select_str = "0";
+    $('*[data-roleid]:checked').each((item)=>{
+        select_str += "," + item.attr('data-roleid')
+        
+    })
+    select_str
+    */
+}
 function select_edit_user(args){
+    args = JSON.parse(args);
+    id = args['id'];
+
     console.log('select',args);
+    $(".role_editor").removeClass("d-none");
+    $(".role_list").removeClass("d-none");
+
+
+
+    $uid = $("#uid-"+id).html();
+    $username = $("#username-"+id).html();
+    $email = $("#email-"+id).html();
+    $alias = $("#alias-"+id).html();
+    $role_ids = $("#role_ids-"+id).html();
+
+
+    $("#view_uid").html($uid);
+    $("#view_username").html($username);
+    $("#view_email").html($email);
+    $("#view_alias").html($alias);
+    $("#edit_role_ids").html($role_ids);
+
+    $('*[data-roleid]').prop('checked',false);
+    $role_ids.split(",").forEach((item)=>{
+        console.log(item);
+        $('*[data-roleid="'+item+'"]').prop('checked',true);        
+    });
+
 }
--- manager/php/pageData/user_roles.php
+++ manager/php/pageData/user_roles.php
@@ -3,7 +3,8 @@
     require_once('../../../.php/class/DB_class.php');
     $db_connection = new db_connection();
 
-    $urls_db = $db_connection->fetchAllQuery("SELECT `uid`,`username`,`email`,`alias`,`role_ids` FROM `users` ORDER BY `uid`");
+    $users = $db_connection->fetchAllQuery("SELECT `uid`,`username`,`email`,`alias`,`role_ids` FROM `users` ORDER BY `uid`");
+    $roles = $db_connection->fetchAllQuery("SELECT * FROM `roles`");
 ?>
 
     <script type="temp">    
@@ -17,10 +18,10 @@
                 <input class="col-5 m-2" type="text">
                 <button class="col-2 m-2 clickEvent noArg" data-function="searchUsers">search</button>
             </div>
-            <div class="col-6">
+            <div class="col-4">
                 <div class="all_urls">
                     <?php
-                        foreach($urls_db as $item){//creates html for each selected database record
+                        foreach($users as $item){//creates html for each selected database record
                     ?>
                         <div class="row p-3">    
                             <?php
@@ -34,7 +35,7 @@
                                 }
                             ?>
                             <div class="row">
-                                <button class="submitEdits clickEvent arg" class="col-3 m-2" data-function="select_edit_user" data-args="{'id':'<?php echo $item["uid"];?>'}">edit this user</button>
+                                <button class="submitEdits clickEvent arg" class="col-3 m-2" data-function="select_edit_user" data-args='{"id":"<?php echo $item["uid"];?>"}'>edit this user</button>
                             </div>
                         </div>
                     <?php
@@ -43,34 +44,52 @@
 
 </div>
             </div>
-            <div class="col-6">
-                <div class="url_editor p-3">
+            <div class="col-5">
+                <div class="role_editor p-3 d-none">
                     <div class="row p-3">    
                         <div class="row">
                             <label class="col-3">uid:</label>
-                            <label id="uid" class="col-9">28</label>
+                            <label id="view_uid" class="col-9"></label>
                         </div>
                         <div class="row">
                             <label class="col-3">username:</label>
-                            <label id="username" class="col-9">STLSHDWS</label>
+                            <label id="view_username" class="col-9"></label>
                         </div>
                         <div class="row">
                             <label class="col-3">email:</label>
-                            <label id="email" class="col-9"></label>
+                            <label id="view_email" class="col-9"></label>
                         </div>
                         <div class="row">
                             <label class="col-3">alias:</label>
-                            <label id="alias" class="col-9">steel</label>
+                            <label id="view_alias" class="col-9"></label>
                         </div>
                         <div class="row">
                             <label class="col-3">role_ids:</label>
-                            <label id="role_ids" class="col-9">1,2</label>
+                            <label id="edit_role_ids" class="col-9"></label>
                         </div>
                     </div>
                     <div class="row p-1">
                         <button class="submitEdits clickEvent noArg" class="col-3 m-2" data-function="submitUserRoles">submit</button>
                         
                     </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="role_list d-none">
+                    <?php
+                        foreach($roles as $item){//creates html for each selected database record
+                    ?>
+                        <div class="row">
+                            <div class="col-1 p-1">
+                                <input type="checkbox" class="clickEvent arg" data-function="addRole" data-roleid="<?php echo $item["role_id"];?>" data-args='{"role_id":"<?php echo $item["role_id"];?>"}'>
+                            </div>
+                            <div class="col">
+                                <label><?php echo $item["role_name"];?></label>
+                            </div>                    
+                        </div>
+                    <?php
+                        }
+                    ?>
                 </div>
             </div>
         </div>
